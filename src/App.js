d7089c4e7106ec85f30262fb2168b35ea4412c55
Dan Khayutin <dan.khayutin@gmail.com>
2018-06-13 17:01:15 -0400

    made inputs dynamic, information only appears when form is correctly filled out

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ const API_KEY = 'b696363142b20d5c2269c851587a3024'
 
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+  }
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -16,14 +24,40 @@ class App extends React.Component {
 
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
     const data = await api_call.json();
-    console.log(data);
+    if(city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      });
+    } else {
+      this.setState({
+        temperature: undefined ,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please Enter The Values!'
+      });
+    }
   }
   render() {
     return (
       <div>
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+         />
       </div>
     );
   }
