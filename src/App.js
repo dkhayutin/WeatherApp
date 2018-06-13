886ef7728d38c98592435c5573a540bcb096c7b6
Dan Khayutin <dan.khayutin@gmail.com>
2018-06-13 18:42:24 -0400

    added bootstrap, css styling, small edits to fonts and colors

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Weather from "./components/weather"
 
 const API_KEY = 'b696363142b20d5c2269c851587a3024'
 
-
-
 class App extends React.Component {
   state = {
     temperature: undefined,
@@ -47,16 +45,28 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Titles />
-        <Form getWeather={this.getWeather}/>
-        <Weather
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error}
-         />
+      <div className='wrapper'>
+        <div className='main'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-xs-5 title-container'>
+                <Titles />
+              </div>
+              <div className='col-xs-7 form-container'>
+                <Form getWeather={this.getWeather}/>
+                <Weather
+                temperature={this.state.temperature}
+                city={this.state.city}
+                country={this.state.country}
+                humidity={this.state.humidity}
+                description={this.state.description}
+                error={this.state.error}
+               />
+              </div>
+            </div>
+          </div>
+        </div>
+       </div>
       </div>
     );
   }
