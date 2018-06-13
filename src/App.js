f7e5f138449da4160c62c1398729d0f1187abf76
Dan Khayutin <dan.khayutin@gmail.com>
2018-06-12 21:25:43 -0400

    set up basic form input data, API call, react components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Titles from "./components/titles";
+import Form from "./components/form";
+import Weather from "./components/weather"
+
+const API_KEY = 'b696363142b20d5c2269c851587a3024'
+
+
+
+class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+    const data = await api_call.json();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
+      </div>
+    );
+  }
+}
+
+export default App;
